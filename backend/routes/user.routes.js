43ab2e71b918b0f71eba3bfb57026bd746edc1c0
VMPRANAV <mailidofpranav@gmail.com>
2025-10-03 12:47:40 +0530

    Feat: Added Update Password Improved UI

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,7 +3,9 @@ import { getUserProfile,updateUserProfile,updateProfilePhoto,updatePassword } fr
 import {protect}from "../middleware/auth.middleware.js"
 import { upload } from "../middleware/upload.middleware.js";
 const router= express.Router();
-router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile);
 router.route('/profile/photo').post(protect, upload.single('photo'), updateProfilePhoto)
+router.use(express.json());
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile);
+
 router.route('/profile/password').post(protect,updatePassword);
 export default router;
