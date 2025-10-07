d02e02ddc0b5cf5e235e20254105e6f59855020a
vadiraj-22 <vadirajjoshi22504@gmail.com>
2025-10-07 19:14:59 +0530

    complaint route and middleware error fixed

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,15 @@ app.use(cors({
   credentials: true
 }));
 
+// Middleware must be defined before routes
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/user/profile/photo", userRoutes);
-app.use(express.json());
-app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user/",userRoutes);
 app.use("/api/complaints",complaintRoutes)
