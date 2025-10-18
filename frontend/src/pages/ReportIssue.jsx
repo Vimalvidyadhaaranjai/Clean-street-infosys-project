4c5798efb0c8319fa5396ca964885ca467d06370
vadiraj-22 <vadirajjoshi22504@gmail.com>
2025-10-18 11:21:59 +0530

    ui changes in report issue

--- frontend/src/pages/ReportIssue.jsx
+++ frontend/src/pages/ReportIssue.jsx
@@ -126,12 +126,12 @@ const ReportIssue = () => {
                             <p className="text-xs text-gray-500 mt-2 font-semibold">{photo.name}</p>
                           </div>
                         ) : (
-                          <div className="space-y-1 text-center">
+                          <div onChange={handlePhotoChange} className="space-y-1 text-center">
                             <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true"><path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" /></svg>
                             <div className="flex text-sm text-gray-600">
                               <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-blue-600 hover:text-blue-700 p-1">
-                                <span>Upload a file</span>
-                                <input id="file-upload" name="photo" type="file" className="sr-only" accept="image/*" onChange={handlePhotoChange} />
+                                <span className="mx-6">Upload a file</span>
+                                <input id="file-upload" name="photo" type="file" className="sr-only" accept="image/*"  />
                               </label>
                             </div>
                             <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
