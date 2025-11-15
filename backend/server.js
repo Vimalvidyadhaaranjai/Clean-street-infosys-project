d32c784907dc6faf04d1e4f262cfdf9f1d734c1f
VMPRANAV <mailidofpranav@gmail.com>
2025-11-15 15:55:13 +0530

    Updated Server

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://infosys-spring-board-clean-p-roject.vercel.app"
+   "https://infosys-cleanstreet.vercel.app/"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
